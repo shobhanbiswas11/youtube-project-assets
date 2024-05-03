428012b026c883ed1179f0887471d74020db9d6c
Shobhan Biswas <61372335+shobhanbiswas11@users.noreply.github.com>
2024-05-03 20:55:44 +0530

    shad-cn init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="container max-w-screen-lg">
+      <Button>Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
